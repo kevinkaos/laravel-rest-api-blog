b921ddb67c8cfc1f342a49674e6f97a237b078f0
Kevin Kao <namedkk126@gmail.com>
2021-06-10 11:38:50 +0800

    import hash facade

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
